cd938b13f710df223c1ee085f84ca5467e841bca
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-02-06 14:59:29 +0800

    before push to remote

--- src/Computer.java
+++ src/Computer.java
@@ -1,6 +1,13 @@
+import java.util.Stack;
+
 /**
  * Created by alvinjay on 2/5/15.
  */
 public class Computer {
     //computeValue
+    public void computeValue (Stack a) {
+        //TODO: compute value
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
+import java.util.Stack;
+
 /**
  * Created by alvinjay on 2/5/15.
  */
 public class Main {
     public static void main(String[] args){
         Parser parser = new Parser();
+        Computer comp = new Computer();
         String sample = "a = 671 * 23 % 3 / 9 - 1 + 3\nb = 3\n3 * 9 / 5 + 1 % 2";
         sample = sample.replace(" ", "");
         String[] temp = sample.split("\n");
 
         for (int i = 0; i < temp.length; i++) {
             String line = temp[i];
-            System.out.println(parser.convertToPostfix(line));
+            Stack temps = parser.convertToPostfix(line);
+            comp.computeValue(temps);
+
         }
     }
 }
